419f4d241ca32149fb6a7e3f96eb9cd42c939e0b
Embbnux <embbnux@embbnux.com>
2022-05-28 23:50:10 +0800

    feat: add x-forwarded header

--- server.js
+++ server.js
@@ -75,6 +75,24 @@ app.get('/tunnel_jwt_generator', (req, res) => {
   res.send('Forbidden');
 });
 
+function getReqHeaders(req) {
+  const encrypted = !!(req.isSpdy || req.connection.encrypted || req.connection.pair);
+  const headers = { ...req.headers };
+  const url = new URL(`${encrypted ? 'https' : 'http'}://${req.headers.host}`);
+  const forwardValues = {
+    for: req.connection.remoteAddress || req.socket.remoteAddress,
+    port: url.port || (encrypted ? 443 : 80),
+    proto: encrypted ? 'https' : 'http',
+  };
+  ['for', 'port', 'proto'].forEach((key) => {
+    const previousValue = req.headers[`x-forwarded-${key}`] || '';
+    headers[`x-forwarded-${key}`] =
+      `${previousValue || ''}${previousValue ? ',' : ''}${forwardValues[key]}`;
+  });
+  headers['x-forwarded-host'] = req.headers['x-forwarded-host'] || req.headers.host || '';
+  return headers;
+}
+
 app.use('/', (req, res) => {
   const tunnelSocket = tunnelSockets[req.headers.host];
   if (!tunnelSocket) {
@@ -88,7 +106,7 @@ app.use('/', (req, res) => {
     requestId,
     request: {
       method: req.method,
-      headers: { ...req.headers },
+      headers: getReqHeaders(req),
       path: req.url,
     },
   });
@@ -164,7 +182,7 @@ httpServer.on('upgrade', (req, socket, head) => {
     requestId,
     request: {
       method: req.method,
-      headers: { ...req.headers },
+      headers: getReqHeaders(req),
       path: req.url,
     },
   });
